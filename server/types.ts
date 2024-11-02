5830ea366d6fa7ab62e1f38994e3734d5e0d6eec
Aarohi Nadkarni <68303030+aarohinadkarni@users.noreply.github.com>
2024-11-02 18:21:16 -0400

    Add follow schema (#2)
    * feat: add follow data types
    
    * refactor: make username what we are tracking
    
    * refactor: make other fields required as well & docs

--- server/types.ts
+++ server/types.ts
@@ -207,6 +207,21 @@ export interface AddCommentRequest extends Request {
 export type CommentResponse = Comment | { error: string };
 
 /**
+ * Interface representing a Follow, which contains:
+ * - _id - The unique identifier for the follower. Optional field.
+ * - followerUsername - The username of the user who followed a user.
+ * - followeeUsername - The username of the user who was followed by a user.
+ * - followDateTime - The date and time when the follow was posted.
+ *
+ */
+export interface Follow {
+  _id?: ObjectId;
+  followerUsername: string;
+  followeeUsername: string;
+  followDateTime: Date;
+}
+
+/**
  * Interface representing the payload for a comment update event, which contains:
  * - result - The updated question or answer.
  * - type - The type of the updated item, either 'question' or 'answer'.

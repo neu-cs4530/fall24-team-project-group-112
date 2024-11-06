2a85bac9e2a608046d062f41344f18b2af3bc72c
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-11-06 13:10:48 -0500

    add delete endpoint

--- server/tests/notifications.spec.ts
+++ server/tests/notifications.spec.ts
@@ -5,38 +5,38 @@ import { app } from '../app';
 import * as util from '../models/application';
 import { Notification, NotificationType } from '../types';
 
-describe('POST /seen/:username', () => {
+const username = 'receiver1';
+const notifications: Notification[] = [
+  {
+    _id: new ObjectId('65e9b58910afe6e94fc6e6de'),
+    notificationType: NotificationType.ANSWER,
+    eventId: new ObjectId('73e9b58910afe6e94fc6e6de'),
+    receiverUsername: username,
+    notificationDate: new Date('2023-11-19T09:24:00'),
+    seen: false,
+  },
+  {
+    _id: new ObjectId('91e9b58910afe6e94fc6e6de'),
+    notificationType: NotificationType.ANSWER,
+    eventId: new ObjectId('75e9b58910afe6e94fc6e6de'),
+    receiverUsername: username,
+    notificationDate: new Date('2023-11-19T09:24:00'),
+    seen: false,
+  },
+  {
+    _id: new ObjectId('91e9b58910afe6e94fc6e6de'),
+    notificationType: NotificationType.ANSWER,
+    eventId: new ObjectId('75e9b58910afe6e94fc6e6de'),
+    receiverUsername: 'receiver2',
+    notificationDate: new Date('2023-11-19T09:24:00'),
+    seen: false,
+  },
+];
+
+describe('PATCH /seen/:username', () => {
   afterAll(async () => {
     await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
   });
-
-  const username = 'receiver1';
-  const notifications: Notification[] = [
-    {
-      _id: new ObjectId('65e9b58910afe6e94fc6e6de'),
-      notificationType: NotificationType.ANSWER,
-      eventId: new ObjectId('73e9b58910afe6e94fc6e6de'),
-      receiverUsername: username,
-      notificationDate: new Date('2023-11-19T09:24:00'),
-      seen: false,
-    },
-    {
-      _id: new ObjectId('91e9b58910afe6e94fc6e6de'),
-      notificationType: NotificationType.ANSWER,
-      eventId: new ObjectId('75e9b58910afe6e94fc6e6de'),
-      receiverUsername: username,
-      notificationDate: new Date('2023-11-19T09:24:00'),
-      seen: false,
-    },
-    {
-      _id: new ObjectId('91e9b58910afe6e94fc6e6de'),
-      notificationType: NotificationType.ANSWER,
-      eventId: new ObjectId('75e9b58910afe6e94fc6e6de'),
-      receiverUsername: 'receiver2',
-      notificationDate: new Date('2023-11-19T09:24:00'),
-      seen: false,
-    },
-  ];
 
   test('markNotificationsAsSeen should update the notifications of the specified user', async () => {
     const expectedResults = notifications
@@ -78,6 +78,45 @@ describe('POST /seen/:username', () => {
     expect(result.status).toBe(500);
     expect(result.text).toEqual(
       'Error when marking notifications as seen: Error when marking notifications as seen: Invalid username',
+    );
+  });
+});
+
+describe('DELETE /:username', () => {
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+
+  test('deleteNotifications should delete the notifications of the specified user', async () => {
+    jest.spyOn(util, 'deleteNotificationsForUser').mockResolvedValueOnce(undefined);
+
+    const result = await supertest(app).delete(`/notification/${username}`);
+
+    expect(result.status).toBe(200);
+  });
+  test('markNotificationsAsSeen should return an error if there is an error updating the notifications', async () => {
+    jest
+      .spyOn(util, 'deleteNotificationsForUser')
+      .mockResolvedValueOnce({ error: 'Error performing delete' });
+
+    const result = await supertest(app).delete(`/notification/${username}`);
+    expect(result.status).toBe(500);
+    expect(result.text).toEqual('Error when deleting notifications: Error performing delete');
+  });
+
+  test('markNotificationsAsSeen should return an error if no username is provided', async () => {
+    const result = await supertest(app).delete(`/notification/${username}`);
+    expect(result.status).toBe(500);
+    expect(result.text).toEqual(
+      'Error when deleting notifications: Error when deleting notifications: Invalid username',
+    );
+  });
+
+  test('markNotificationsAsSeen should return an error if an invalid username is provided', async () => {
+    const result = await supertest(app).delete(`/notification/${username}`);
+    expect(result.status).toBe(500);
+    expect(result.text).toEqual(
+      'Error when deleting notifications: Error when deleting notifications: Invalid username',
     );
   });
 });

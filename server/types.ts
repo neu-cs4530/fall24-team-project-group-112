ea2bc001fe8bf7c4a500b276646c14011ae8d35b
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-11-07 10:36:18 -0500

    create filtered endpoint

--- server/types.ts
+++ server/types.ts
@@ -324,6 +324,20 @@ export interface Notification {
 }
 
 /**
+ * Interface extending the request body when retrieving a user's notification, which contains:
+ * - username - The username of the user whose notifications are being retrieved.
+ * - type - The type of notification to retrieve.
+ */
+export interface GetNotificationRequest extends Request {
+  params: {
+    username: string;
+  };
+  query: {
+    type: NotificationType;
+  };
+}
+
+/**
  * Type representing the possible responses for a User-related operation.
  */
 export type UserResponse = User | { error: string };

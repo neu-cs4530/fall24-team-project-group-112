9b0f8e6e9e205a24431d05558153bd39a9baed72
Liang jimenez <liangjimenez@yahoo.com>
2024-11-14 12:51:49 -0500

    lint error

--- client/src/hooks/useProfile.ts
+++ client/src/hooks/useProfile.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { Follow, User } from '../types';
 import { getFollowers, getUser, addFollow } from '../services/userService';

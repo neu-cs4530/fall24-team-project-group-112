5ba658167fc58ddb137939b87733b1ff072852b4
aarohinadkarni <arunadkarni@gmail.com>
2024-11-14 02:34:09 -0500

    feat: all functionality without styling

--- client/src/components/main/activityPages/notificationPage/index.tsx
+++ client/src/components/main/activityPages/notificationPage/index.tsx
@@ -5,10 +5,14 @@ import NotificationFilter from './filterComponent';
 import './index.css';
 
 const NotificationCenter: React.FC = () => {
-  const { notifications, error, setNotificationType } = useNotifications();
+  const { notifications, error, setNotificationType, deleteNotifications } = useNotifications();
 
   const handleFilterChange = (type: string) => {
     setNotificationType(type);
+  };
+
+  const handleClearNotifications = () => {
+    deleteNotifications();
   };
 
   if (error) {
@@ -18,6 +22,14 @@ const NotificationCenter: React.FC = () => {
   return (
     <div className='notification-div'>
       <h2 className='notification-title font-bold text-4xl'>Notifications Center</h2>
+
+      <div className='text-right'>
+        <button
+          className='mb-5 border border-black rounded px-6 py-5'
+          onClick={handleClearNotifications}>
+          Clear all notifications
+        </button>
+      </div>
 
       <div className='mb-5'>
         <NotificationFilter onFilterChange={handleFilterChange} />

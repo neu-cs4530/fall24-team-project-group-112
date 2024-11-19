d3e6fad47494890268ea367bdde1dbee126bd8b4
neetidesai <97416423+neetidesai@users.noreply.github.com>
2024-11-19 10:14:16 -0500

    fixups

--- client/src/components/main/questionPage/question/index.tsx
+++ client/src/components/main/questionPage/question/index.tsx
@@ -74,6 +74,7 @@ const QuestionView = ({ q }: QuestionProps) => {
       </div>
       <div className='lastActivity'>
         <div className='question_author'>{q.askedBy}</div>
+        <div>&nbsp;</div>
         <div className='question_meta'>asked {getMetaData(new Date(q.askDateTime))}</div>
       </div>
     </div>

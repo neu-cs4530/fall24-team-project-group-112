ad7c15c0a177112b6aa000449f6213a4013b996b
neetidesai <97416423+neetidesai@users.noreply.github.com>
2024-11-25 18:28:04 -0500

    Move styles to const

--- client/src/components/main/profile/index.tsx
+++ client/src/components/main/profile/index.tsx
@@ -61,22 +61,30 @@ const Profile = ({ loggedInUser }: { loggedInUser: User | null }) => {
     );
   }
 
+  const styles = {
+    loadingContainer: 'flex justify-center items-center h-screen',
+    container: 'flex justify-center',
+    badgeContainerOuter: 'mt-6',
+    badgeContainerInner: 'flex',
+    badgeText: 'font-bold text-xl ml-4 pt-1',
+    badgeButton:
+      'ml-5 bg-stackpurple text-white text-sm hover:bg-stackpurplehover rounded-md p-2 cursor-pointer',
+  };
+
   return (
     <>
       {user && !isLoading ? (
-        <div className='flex justify-center'>
+        <div className={styles.container}>
           <div>
-            <div className='flex justify-center'>
+            <div className={styles.container}>
               <ProfileText user={user} loggedInUser={loggedInUser} />
             </div>
             <div>
               {user.badges.length > 0 && (
-                <div className='mt-6'>
-                  <div className='flex'>
-                    <h2 className='font-bold text-xl ml-4 pt-1'>Badges</h2>
-                    <p
-                      className='ml-5 bg-stackpurple text-white text-sm hover:bg-stackpurplehover rounded-md p-2 cursor-pointer'
-                      onClick={() => setBadgeOpen(true)}>
+                <div className={styles.badgeContainerOuter}>
+                  <div className={styles.badgeContainerInner}>
+                    <h2 className={styles.badgeText}>Badges</h2>
+                    <p className={styles.badgeButton} onClick={() => setBadgeOpen(true)}>
                       View all badges
                     </p>
                   </div>
@@ -104,7 +112,7 @@ const Profile = ({ loggedInUser }: { loggedInUser: User | null }) => {
           </div>
         </div>
       ) : (
-        <div className='flex justify-center items-center h-screen'>
+        <div className={styles.loadingContainer}>
           <CircularProgress />
         </div>
       )}

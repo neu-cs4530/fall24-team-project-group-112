1905597f582b2a19a8c1f0bf8e789b4ec1f29b1c
neetidesai <97416423+neetidesai@users.noreply.github.com>
2024-12-01 21:57:28 -0500

    Spelling fixup

--- server/types.ts
+++ server/types.ts
@@ -323,7 +323,7 @@ export interface ServerToClientEvents {
   voteUpdate: (vote: VoteUpdatePayload) => void;
   commentUpdate: (comment: CommentUpdatePayload) => void;
   notificationUpdate: (notification: Notification) => void;
-  notificationDelete: (notificatioId: string) => void;
+  notificationDelete: (notificationId: string) => void;
   profileUpdate: (user: User) => void;
   followUpdate: (follow: String) => void;
 }
